38883ac3461f55c3da35141f40d6e7b43203e191
leehom-liulihong <llh_01129@163.com>
2024-06-13 13:06:04 +0800

    fix: 日志整改 (#23)
    * 去掉转义字符的功能修改，因为会引入图片格式被转码的问题
    
    * 日志整改

--- harmony/fast_image/src/main/cpp/FastImageViewComponentInstance.cpp
+++ harmony/fast_image/src/main/cpp/FastImageViewComponentInstance.cpp
@@ -89,15 +89,15 @@ std::string urlEncode(const std::string &str) {
 
 void FastImageViewComponentInstance::onPropsChanged(SharedConcreteProps const &props) {
     CppComponentInstance::onPropsChanged(props);
-    LOG(INFO) << "[FastImage] Props->tinColor: " << props->tintColor;
-    LOG(INFO) << "[FastImage] Props->source.uri: " << props->source.uri;
-    LOG(INFO) << "[FastImage] Props->resizeMode: " << facebook::react::toString(props->resizeMode);
-    LOG(INFO) << "[FastImage] Props->defaultSource: " << props->defaultSource;
+    DLOG(INFO) << "[FastImage] Props->tinColor: " << props->tintColor;
+    DLOG(INFO) << "[FastImage] Props->source.uri: " << props->source.uri;
+    DLOG(INFO) << "[FastImage] Props->resizeMode: " << facebook::react::toString(props->resizeMode);
+    DLOG(INFO) << "[FastImage] Props->defaultSource: " << props->defaultSource;
 
     if (!props->source.headers.empty()) {
         for (auto header : props->source.headers) {
-            LOG(INFO) << "[FastImage] Props->headers.name: " << header.name;
-            LOG(INFO) << "[FastImage] Props->headers.value: " << header.value;
+            DLOG(INFO) << "[FastImage] Props->headers.name: " << header.name;
+            DLOG(INFO) << "[FastImage] Props->headers.value: " << header.value;
         }
     }
 
