6e00ced9a93542a71ba4cea11fc8e6b3bf5555f6
cmdssd <168167043+cmdssd@users.noreply.github.com>
2024-08-09 17:29:48 +0800

    fix:修复ctx里获取不到arkTSMessageHub的问题 (#45)
    Co-authored-by: kongchuiyu <kongchuiyu@h-partners.com>

--- harmony/fast_image/src/main/cpp/FastImageLoaderTurboModule.cpp
+++ harmony/fast_image/src/main/cpp/FastImageLoaderTurboModule.cpp
@@ -1,5 +1,6 @@
 #include "FastImageLoaderTurboModule.h"
 #include "RNOH/ArkTSTurboModule.h"
+#include "RNOH/RNInstanceCAPI.h"
 
 using namespace rnoh;
 using namespace facebook;
@@ -17,21 +18,14 @@ rnoh::FastImageLoaderTurboModule::FastImageLoaderTurboModule(
       ARK_ASYNC_METHOD_METADATA(prefetchImageWithMetadata, 3),
       ARK_ASYNC_METHOD_METADATA(queryCache, 1)};
     
-    m_ctx.taskExecutor->runTask(
-      TaskThread::MAIN, [weakInstance = m_ctx] {
-        auto instance = weakInstance.instance.lock();
-        if (!instance) {
-          return;
-        }
-        auto self = instance->getTurboModule<FastImageLoaderTurboModule>("FastImageLoader");
-        if (!self) {
-          return;
-        }
-        if (self->m_FastImageSourceResolver) {
-          return;
-        }
-        
-        self->m_FastImageSourceResolver = std::make_shared<FastImageSourceResolver>(
-            weakInstance.arkTSMessageHub, instance);
-    });
+    if(m_FastImageSourceResolver){
+        return;
+    }
+
+    if(auto instance = m_ctx.instance.lock(); instance != nullptr){
+        m_FastImageSourceResolver = std::make_shared<FastImageSourceResolver>(instance);
+        auto rnInstance = dynamic_cast<RNInstanceCAPI*>(instance.get());
+        rnInstance->addArkTSMessageHandler(m_FastImageSourceResolver);
+    }
+    
 }
