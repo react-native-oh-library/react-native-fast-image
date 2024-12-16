a3c15756f1f046eb4e4129261ac6f96d962b9f5b
kongchuiyu <kongchuiyu@h-partners.com>
2024-12-16 11:48:09 +0800

    chore: add license header and format code style

--- harmony/fast_image/src/main/ets/FastRemoteImageLoader/RemoteImageLoader.ts
+++ harmony/fast_image/src/main/ets/FastRemoteImageLoader/RemoteImageLoader.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import image from '@ohos.multimedia.image';
 import type { RemoteImageMemoryCache } from './RemoteImageCache';
 import { RemoteImageLoaderError } from './RemoteImageLoaderError';
